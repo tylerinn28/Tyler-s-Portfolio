71c3619f1402f348def97bf2c9977ba5c735afb3
tylerinn28 <tylerinn28@gmail.com>
2020-09-22 13:13:16 -1000

    viewport in indexhtml

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -12,7 +12,6 @@ export const HomePage = () => {
      content='width=device-width, initial-scale=1.0, maximum-scale=1.0, 
      user-scalable=0' ></meta>
 
-
 <div className="content">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
 
